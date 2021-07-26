c0de014aa3c5be2b30d4765500a23f67505e39bc
Zack <Home@MacBook-Air.local>
2021-07-26 01:34:39 -0400

    Update .env

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -5,13 +5,13 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCG1VNgRezLrIw2yT0rxZr-Y9EwhLyAtFQ',
+  apiKey: `${process.env.VUE_APP_FIREBASEAPP_API_KEY}`,
   authDomain: 'workoutstracker.firebaseapp.com',
   projectId: 'workoutstracker',
   storageBucket: 'workoutstracker.appspot.com',
-  messagingSenderId: '273010277268',
-  appId: '1:273010277268:web:303120b00acc7786912d81',
-  measurementId: 'G-BS98W8C68G'
+  messagingSenderId: `${process.env.VUE_APP_MESSAGEING_SENDER_ID}`,
+  appId: `${process.env.VUE_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.VUE_APP_MEASUREMENT_ID}`
 })
 
 export default firebaseApp.firestore()
